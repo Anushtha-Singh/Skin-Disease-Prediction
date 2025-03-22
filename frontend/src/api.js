d2b43e9220f3687f3c4a60afb6c08906ea44b7f3
Anushtha <anushthasinghK@gmail.com>
2025-03-22 14:18:34 +0530

    changing the url to correct api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/predict/";  // FastAPI backend URL
-
+//const API_URL = "http://127.0.0.1:8000/predict/";  // FastAPI backend URL
+const API_URL = "https://skin-disease-prediction-lfhq.onrender.com/predict";
 export const uploadImage = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
