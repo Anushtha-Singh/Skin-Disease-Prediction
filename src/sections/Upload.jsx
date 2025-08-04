cc4e66ed1e855a5e3804dc05c1f85fccd539c1f8
Anushtha Singh Kushwah <anushthasinghk@gmail.com>
2025-08-04 23:30:17 +0530

    adding deployed backend link

--- src/sections/Upload.jsx
+++ src/sections/Upload.jsx
@@ -80,7 +80,7 @@ const Upload = () => {
       const blob = await res.blob();
       formData.append("file", blob, "image.jpg");
 
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("https://derma-skin-scan-backend.onrender.com", {
         method: "POST",
         body: formData,
       });
